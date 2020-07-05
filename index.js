140f0a072dc53bc718f75208f935780fd13caf9e
abrilgarciaduran <abrilgarciaduran@gmail.com>
2020-07-04 21:10:47 -0300

    more methods

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
 
 const inventors = [
     { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
@@ -106,3 +120,49 @@ const transportes = data.reduce(function (counting, item) {
 console.log("En el array de transportes hay: ");
 console.log(transportes)
 
+
+// 9. Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const atLeastOneAdult = people.some(function (person) {
+    const actualYear = (new Date()).getFullYear();
+    return actualYear - person.year >= 19
+})
+console.log('Working with...')
+console.table(people)
+console.log(`Are there any adults? ${atLeastOneAdult}`)
+
+// 10. Array.prototype.every() // is everyone 19 or older?
+const allAdults = people.every(function (person) {
+    const actualYear = (new Date()).getFullYear()
+    return actualYear - person.year >= 19
+})
+console.log(`Are them all adults? ${allAdults}`)
+
+// 11. Array.prototype.find()
+// find the comment with the ID of 823423
+const comment = comments.find(comment => (comment.id == 823423))
+console.log('Working with...')
+console.table(comments)
+console.log(`The comment with ID = 823423 is`)
+console.log(comment)
+
+// 12. Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const index = comments.findIndex(comment => (comment.id == 823423))
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+]
+
+// 13. Deleting by id (creates a new array without the comment with the given id)
+console.log('You can delete a comment with deleteCommentById(id)')
+function deleteCommentById(id) {
+    const index = comments.findIndex(comment => (comment.id == id))
+    console.log(index)
+    const newComments = [
+        ...comments.slice(0, index),
+        ...comments.slice(index + 1)
+    ]
+    console.table(newComments)
+}
